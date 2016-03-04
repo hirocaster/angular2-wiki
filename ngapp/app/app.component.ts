34aad14769b74f6accd10d65745098083e1b9cc6
hirocaster <hohtsuka@gmail.com>
2016-03-04 22:06:43 +0900

    Add angular2 app

---
+++ ngapp/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "angular2/core";
+
+@Component({
+  selector: "my-app",
+  template: `<h1>Hello App</h1>`,
+  styleUrls: [],
+  providers: []
+})
+
+export class AppComponent {
+}
