efbcb1c41b8bd6473ae41bb23e9125aa9bf86f5a
hirocaster <hohtsuka@gmail.com>
2016-03-07 17:47:37 +0900

    Fix lint warning

--- ngapp/src/app/item.service.ts
+++ ngapp/src/app/item.service.ts
@@ -41,7 +41,7 @@ export class ItemService {
   getCookie(name) {
     let value = "; " + document.cookie;
     let parts = value.split("; " + name + "=");
-    if (parts.length == 2)
+    if (parts.length === 2)
       return parts.pop().split(";").shift();
   }
 
